2d50c2261e2eb51793ead2191413d98af2795bb6
Lokeshwar_V <lokeshwar619@gmail.com>
2024-10-15 19:28:53 +0530

    major change

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,68 +1,3 @@
-"use client";
-import React, { useTransition, useState } from "react";
-import Image from "next/image";
-import TabButton from "./TabButton";
-import { content } from "../../../tailwind.config";
-
-const TAB_DATA = [
-  {
-    title: "Skills",
-    id: "skills",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>React.js</li>
-        <li>Node.js</li>
-        <li>Html-css-JavaScript</li>
-        <li>Flutter</li>
-        <li>Java</li>
-        <li>Python</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Education",
-    id: "education",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>
-          2022|B.Tech Information Technology - Vel Tech Multi Tech Dr
-          Rangarajan Dr Shakunthala College <p>Graduated with 81.5%</p>{" "}
-        </li>
-        <li>
-          2018|12th/HSC-Sri R M Jain Vidhyapeeth Higher Secondary School{" "}
-          <p>Graduated with 82.5%</p>{" "}
-        </li>
-        <li>
-          2016|10th/SSLC-Sri R M Jain Vidhyasharam CBSE
-          <p>Graduated with 80%</p>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Full Stack with Java training - Beasant Technologies</li>
-        <li>
-          The Complete 2021 Flutter Development Bootcamp with Dart - Udemy
-        </li>
-        <li>The Complete 2021 Web Development Bootcamp - Udemy</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Work Experience",
-    id: "work",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>2023Dec-Present|Software Developer Intern - NYL Technologies</li>
-      </ul>
-    ),
-  },
-];
-
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
@@ -87,41 +22,37 @@ const AboutSection = () => {
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Node.js, Express , HTML and CSS. I'm
-            currently working at Accenture as a packaged application development
-            Associate. I have also published 3 flutter apps on Amazon app store
-            I am a quick learner and I am always looking to expand my knowledge
-            and skill set. I am a team player and I am excited to work with
-            others to create amazing applications.
+            working with JavaScript, React, Node.js, Express , HTML and CSS.
+            I&apos;m currently working at Accenture as a packaged application
+            development Associate. I have also published 3 flutter apps on
+            Amazon app store. I am a quick learner and I am always looking to
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
             >
-              {" "}
-              Skills{" "}
+              Skills
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("education")}
               active={tab === "education"}
             >
-              {" "}
-              Education{" "}
+              Education
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("certifications")}
               active={tab === "certifications"}
             >
-              {" "}
-              Certifications{" "}
+              Certifications
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("work")}
               active={tab === "work"}
             >
-              {" "}
-              Work Experience{" "}
+              Work Experience
             </TabButton>
           </div>
           <div className="mt-8">
